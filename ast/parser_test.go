IxDay/opa

commit 93356bbdfc0a5023e5b8a0b0f0bca67b9261523f

Fix test cases to work with new syntax

ast/parser_ext.go
@@ -118,7 +118,7 @@ func MustParseTerm(input string) *Term {
 
 // ParseRuleFromBody attempts to return a rule from a body. Equality expressions
 // of the form <var> = <term> can be converted into rules of the form <var> =
-// <term> :- true.  This is a concise way of defining constants inside modules.
+// <term> { true }. This is a concise way of defining constants inside modules.
 func ParseRuleFromBody(body Body) (*Rule, error) {
 
 	if len(body) != 1 {

server/server_test.go
@@ -48,45 +48,39 @@ type tr struct {
 func TestDataV1(t *testing.T) {
 
 	testMod1 := `package testmod
-                p[x] :- q[x], not r[x]
-                q[x] :- data.x.y[i] = x
-                r[x] :- data.x.z[i] = x
 
-				import input.req1
-				import input.req2 as reqx
-				import input.req3.attr1
-				g :- req1.a[0] = 1, reqx.b[i] = 1
-				h :- attr1[i] > 1
+import input.req1
+import input.req2 as reqx
+import input.req3.attr1
 
-				gt1 :- req1 > 1
-				arr = [1,2,3,4]
-
-				undef :- false
-				`
+p[x] { q[x]; not r[x] }
+q[x] { data.x.y[i] = x }
+r[x] { data.x.z[i] = x }
+g = true { req1.a[0] = 1; reqx.b[i] = 1 }
+h = true { attr1[i] > 1 }
+gt1 = true { req1 > 1 }
+arr = [1, 2, 3, 4] { true }
+undef = true { false }`
 
 	testMod2 := `package testmod
 
-	p = [1,2,3,4]
-	q = {"a": 1, "b": 2}
-	`
+p = [1, 2, 3, 4] { true }
+q = {"a": 1, "b": 2} { true }`
 
 	testMod3 := `package testmod
 
-	p :- loopback with input as true
-	loopback = input
-	`
+p = true { loopback with input as true }
+loopback = input { true }`
 
 	testMod4 := `package testmod
 
-	p = true :- true
-	p = false :- true
-	`
+p = true { true }
+p = false { true }`
 
 	testMod5 := `package testmod.empty.mod`
 	testMod6 := `package testmod.all.undefined
 
-	p :- false
-	`
+p = true { false }`
 
 	tests := []struct {
 		note string
@@ -378,8 +372,8 @@ func TestDataGetExplainTruth(t *testing.T) {
 	f := newFixture(t)
 
 	f.v1("PUT", "/policies/test", `package test
-	p :- a = [1,2,3,4], a[_] = x, x > 1
-	`, 204, "")
+
+p = true { a = [1, 2, 3, 4]; a[_] = x; x > 1 }`, 204, "")
 
 	req := newReqV1("GET", "/data/test/p?explain=truth", "")
 	f.reset()
@@ -419,7 +413,7 @@ func TestDataPostExplain(t *testing.T) {
 
 	f.v1("PUT", "/policies/test", `package test
 
-	p = [1,2,3,4]`, 200, "")
+p = [1, 2, 3, 4] { true }`, 200, "")
 
 	req := newReqV1("POST", "/data/test/p?explain=full", "")
 	f.reset()
@@ -561,20 +555,18 @@ func TestPoliciesPutV1ParseError(t *testing.T) {
 		t.Fatalf("Unexpected JSON decode error: %v", err)
 	}
 
-	expected := ast.NewLocation(nil, "test", 4, 8)
-
-	if !reflect.DeepEqual(errs.Errors[0].Location, expected) {
-		t.Fatalf("Expected error location to be %v but got: %v", expected, errs)
+	if errs.Errors[0].Location.File != "test" || errs.Errors[0].Location.Row != 4 {
+		t.Fatalf("Bad location: %v (expecfted test:4)", errs)
 	}
 }
 
 func TestPoliciesPutV1CompileError(t *testing.T) {
 	f := newFixture(t)
-	req := newReqV1("PUT", "/policies/test", `
-    package a.b.c
-    p[x] :- q[x]
-    q[x] :- p[x]
-    `)
+	req := newReqV1("PUT", "/policies/test", `package a.b.c
+
+p[x] { q[x] }
+q[x] { p[x] }`,
+	)
 
 	f.server.Handler.ServeHTTP(f.recorder, req)
 
@@ -587,8 +579,6 @@ func TestPoliciesPutV1CompileError(t *testing.T) {
 		t.Fatalf("Unexpected JSON decode error: %v", err)
 	}
 
-	expected := ast.NewLocation(nil, "test", 3, 5)
-
 	if len(errs.Errors) != 2 {
 		t.Fatalf("Expected exactly two errors but got %d: %v", len(errs.Errors), errs)
 	}
@@ -596,13 +586,14 @@ func TestPoliciesPutV1CompileError(t *testing.T) {
 	found := false
 
 	for _, err := range errs.Errors {
-		if reflect.DeepEqual(err.Location, expected) {
+		if err.Location.File == "test" && err.Location.Row == 3 {
 			found = true
+			break
 		}
 	}
 
 	if !found {
-		t.Fatalf("Missing expected error %v: %v", expected, errs)
+		t.Fatalf("Missing expected error %v (expected test:3)", errs)
 	}
 }
 
@@ -726,7 +717,7 @@ func TestPoliciesDeleteV1(t *testing.T) {
 
 func TestQueryV1(t *testing.T) {
 	f := newFixture(t)
-	get := newReqV1("GET", `/query?q=a=[1,2,3],a[i]=x`, "")
+	get := newReqV1("GET", `/query?q=a=[1,2,3]%3Ba[i]=x`, "")
 	f.server.Handler.ServeHTTP(f.recorder, get)
 
 	if f.recorder.Code != 200 {
@@ -754,7 +745,7 @@ func TestQueryV1(t *testing.T) {
 
 func TestQueryV1Explain(t *testing.T) {
 	f := newFixture(t)
-	get := newReqV1("GET", `/query?q=a=[1,2,3],a[i]=x&explain=full`, "")
+	get := newReqV1("GET", `/query?q=a=[1,2,3]%3Ba[i]=x&explain=full`, "")
 	f.server.Handler.ServeHTTP(f.recorder, get)
 
 	if f.recorder.Code != 200 {
@@ -771,7 +762,7 @@ func TestQueryV1Explain(t *testing.T) {
 		t.Fatalf("Expected exactly 10 trace events for full query but got %d", len(result.Explanation))
 	}
 
-	get = newReqV1("GET", "/query?q=a=[1,2,3],a[_]=x,x>1&explain=truth", "")
+	get = newReqV1("GET", "/query?q=a=[1,2,3]%3Ba[_]=x%3Bx>1&explain=truth", "")
 	f.reset()
 	f.server.Handler.ServeHTTP(f.recorder, get)
 
@@ -850,12 +841,13 @@ func TestQueryBindingIterationError(t *testing.T) {
 }
 
 const (
-	testMod = `
-    package a.b.c
-    import data.x.y as z
-    import data.p
-    q[x] :- p[x], not r[x]
-    r[x] :- z[x] = 4`
+	testMod = `package a.b.c
+
+import data.x.y as z
+import data.p
+
+q[x] { p[x]; not r[x] }
+r[x] { z[x] = 4 }`
 )
 
 type fixture struct {

ast/compare_test.go
@@ -32,7 +32,7 @@ func TestCompare(t *testing.T) {
 		{`{1: 2, 3: 4, 5: 6}`, `{1: 2, 3: 4}`, 1},
 
 		// Array comprehensions
-		{`[ null | true ]`, `[ false | null ]`, -1},
+		{`[null | true]`, `[false | null]`, -1},
 
 		// Expressions
 		{`a = b`, `b = a`, -1},
@@ -44,8 +44,8 @@ func TestCompare(t *testing.T) {
 		{`a = b with input.foo as bar`, `a = b with input.foo as bar with input.baz as qux`, -1},
 
 		// Body
-		{`a = b`, `a = b, b = a`, -1},
-		{`a = b, b = a`, `a = b`, 1},
+		{`a = b`, `a = b; b = a`, -1},
+		{`a = b; b = a`, `a = b`, 1},
 	}
 	for _, tc := range tests {
 		var a, b interface{}
@@ -72,9 +72,11 @@ func TestCompareModule(t *testing.T) {
 	}
 
 	a = MustParseModule(`package a.b.c
-    import input.x.y`)
+
+import input.x.y`)
 	b = MustParseModule(`package a.b.c
-    import input.x.z`)
+
+import input.x.z`)
 	result = Compare(a, b)
 
 	if result != -1 {

ast/compile.go
@@ -47,9 +47,9 @@ type Compiler struct {
 	// following module:
 	//
 	//  package ex
-	//  p[1] :- true
-	//  p[2] :- true
-	//  q :- true
+	//  p[1] { true }
+	//  p[2] { true }
+	//  q = true
 	//
 	//  root
 	//    |
@@ -207,8 +207,8 @@ func (c *Compiler) Failed() bool {
 //
 //	package a.b.c
 //
-//	p[k] = v :- ...    # rule1
-//  p[k1] = v1 :- ...  # rule2
+//	p[k] = v { ... }    # rule1
+//  p[k1] = v1 { ... }  # rule2
 //
 // The following calls yield the rules on the right.
 //
@@ -235,8 +235,8 @@ func (c *Compiler) GetRulesExact(ref Ref) (rules []*Rule) {
 //
 //	package a.b.c
 //
-//	p[k] = v :- ...    # rule1
-//  p[k1] = v1 :- ...  # rule2
+//	p[k] = v { ... }    # rule1
+//  p[k1] = v1 { ... }  # rule2
 //
 // The following calls yield the rules on the right.
 //
@@ -266,9 +266,9 @@ func (c *Compiler) GetRulesForVirtualDocument(ref Ref) (rules []*Rule) {
 //
 //  package a.b.c
 //
-//  p[x] = y :- ...  # rule1
-//  p[k] = v :- ...  # rule2
-//  q :- ...         # rule3
+//  p[x] = y { ... }  # rule1
+//  p[k] = v { ... }  # rule2
+//  q { ... }         # rule3
 //
 // The following calls yield the rules on the right.
 //
@@ -306,8 +306,8 @@ func (c *Compiler) GetRulesWithPrefix(ref Ref) (rules []*Rule) {
 //
 //  package a.b.c
 //
-//  p[x] = y :- q[x] = y, ... # rule1
-//  q[x] = y :- ...           # rule2
+//  p[x] = y { q[x] = y; ... } # rule1
+//  q[x] = y { ... }           # rule2
 //
 // The following calls yield the rules on the right.
 //
@@ -516,7 +516,7 @@ func (c *Compiler) getExports() *util.HashMap {
 //
 // package a.b
 // import data.foo.bar
-// p[x] :- bar[_] = x
+// p[x] { bar[_] = x }
 //
 // The reference "bar[_]" would be resolved to "data.foo.bar[_]".
 func (c *Compiler) resolveAllRefs() {
@@ -574,11 +574,11 @@ func (c *Compiler) resolveAllRefs() {
 //
 // For instance, given the following rule:
 //
-// p[{"foo": data.foo[i]}] :- i < 100
+// p[{"foo": data.foo[i]}] { i < 100 }
 //
 // The rule would be re-written as:
 //
-// p[__local0__] :- i < 100, __local0__ = {"foo": data.foo[i]}
+// p[__local0__] { i < 100; __local0__ = {"foo": data.foo[i]} }
 func (c *Compiler) rewriteRefsInHead() {
 	for _, mod := range c.Modules {
 		generator := newLocalVarGenerator(mod)

ast/example_test.go
@@ -13,18 +13,13 @@ import (
 func ExampleCompiler_Compile() {
 
 	// Define an input module that will be compiled.
-	exampleModule := `
+	exampleModule := `package opa.example
 
-		package opa.example
+import data.foo
+import input.bar
 
-		import data.foo
-		import input.bar
-
-		p[x] :- foo[x], not bar[x], x >= min_x
-
-		min_x = 100
-
-	`
+p[x] { foo[x]; not bar[x]; x >= min_x }
+min_x = 100 { true }`
 
 	// Parse the input module to obtain the AST representation.
 	mod, err := ast.ParseModule("my_module", exampleModule)
@@ -57,18 +52,13 @@ func ExampleCompiler_Compile() {
 func ExampleQueryCompiler_Compile() {
 
 	// Define an input module that will be compiled.
-	exampleModule := `
+	exampleModule := `package opa.example
 
-		package opa.example
+import data.foo
+import input.bar
 
-		import data.foo
-		import input.bar
-
-		p[x] :- foo[x], not bar[x], x >= min_x
-
-		min_x = 100
-
-	`
+p[x] { foo[x]; not bar[x]; x >= min_x }
+min_x = 100 { true }`
 
 	// Parse the input module to obtain the AST representation.
 	mod, err := ast.ParseModule("my_module", exampleModule)
@@ -98,13 +88,13 @@ func ExampleQueryCompiler_Compile() {
 			// ast.Parse<X> functions that return meaningful error messages
 			// instead.
 			ast.NewQueryContext().
-				WithPackage(ast.MustParsePackage("package opa.example")).
+				WithPackage(ast.MustParsePackage(`package opa.example`)).
 				WithImports(ast.MustParseImports("import input.query_arg")).
 				WithInput(ast.MustParseTerm(`{"query_arg": 1000, "bar": [1,2,3]}`).Value),
 		)
 
 	// Parse the input query to obtain the AST representation.
-	query, err := ast.ParseBody("p[x], x < query_arg")
+	query, err := ast.ParseBody(`p[x]; x < query_arg`)
 	if err != nil {
 		fmt.Println("Parse error:", err)
 	}
@@ -118,5 +108,5 @@ func ExampleQueryCompiler_Compile() {
 
 	// Output:
 	//
-	// Compiled: data.opa.example.p[x], x < input.query_arg
+	// Compiled: data.opa.example.p[x]; x < input.query_arg
 }

commit d2358659d5c7c1d8952f5a5888aa97f1da2ad9d3

ast: Fix locations on rules parsed from exprs
The location info on rules parsed from exprs was not ideal--it was
being set to the value of the rhs term (which is not accurate.)
This commit updates the parser helper function to reset the location
to the original expr location.

This commit also fixes an issue in the parser helper test case that
was introduced in 93356bbdfc0a5023e5b8a0b0f0bca67b9261523f when we
used a tool to roundtrip the policy examples in the OPA codebase
to move away from the old datalog if-then syntax (":-").

Signed-off-by: Torin Sandall <torinsandall@gmail.com>

ast/parser_ext.go
@@ -167,6 +167,8 @@ func ParseRuleFromExpr(module *Module, expr *Expr) (*Rule, error) {
 		rule, err := ParseCompleteDocRuleFromAssignmentExpr(module, lhs, rhs)
 
 		if err == nil {
+			rule.Location = expr.Location
+			rule.Head.Location = expr.Location
 			return rule, nil
 		} else if _, ok := lhs.Value.(Call); ok {
 			return nil, errFunctionAssignOperator
@@ -178,20 +180,7 @@ func ParseRuleFromExpr(module *Module, expr *Expr) (*Rule, error) {
 	}
 
 	if expr.IsEquality() {
-
-		lhs, rhs := expr.Operand(0), expr.Operand(1)
-		rule, err := ParseCompleteDocRuleFromEqExpr(module, lhs, rhs)
-
-		if err == nil {
-			return rule, nil
-		}
-
-		rule, err = ParseRuleFromCallEqExpr(module, lhs, rhs)
-		if err == nil {
-			return rule, nil
-		}
-
-		return ParsePartialObjectDocRuleFromEqExpr(module, lhs, rhs)
+		return parseCompleteRuleFromEq(module, expr)
 	}
 
 	if _, ok := BuiltinMap[expr.Operator().String()]; ok {
@@ -199,6 +188,33 @@ func ParseRuleFromExpr(module *Module, expr *Expr) (*Rule, error) {
 	}
 
 	return ParseRuleFromCallExpr(module, expr.Terms.([]*Term))
+}
+
+func parseCompleteRuleFromEq(module *Module, expr *Expr) (rule *Rule, err error) {
+
+	// ensure the rule location is set to the expr location
+	// the helper functions called below try to set the location based
+	// on the terms they've been provided but that is not as accurate.
+	defer func() {
+		if rule != nil {
+			rule.Location = expr.Location
+			rule.Head.Location = expr.Location
+		}
+	}()
+
+	lhs, rhs := expr.Operand(0), expr.Operand(1)
+	rule, err = ParseCompleteDocRuleFromEqExpr(module, lhs, rhs)
+
+	if err == nil {
+		return rule, nil
+	}
+
+	rule, err = ParseRuleFromCallEqExpr(module, lhs, rhs)
+	if err == nil {
+		return rule, nil
+	}
+
+	return ParsePartialObjectDocRuleFromEqExpr(module, lhs, rhs)
 }
 
 // ParseCompleteDocRuleFromAssignmentExpr returns a rule if the expression can
@@ -231,9 +247,9 @@ func ParseCompleteDocRuleFromEqExpr(module *Module, lhs, rhs *Term) (*Rule, erro
 	}
 
 	rule := &Rule{
-		Location: rhs.Location,
+		Location: lhs.Location,
 		Head: &Head{
-			Location: rhs.Location,
+			Location: lhs.Location,
 			Name:     name,
 			Value:    rhs,
 		},

ast/parser_test.go
@@ -1818,12 +1818,12 @@ func TestLocation(t *testing.T) {
 func TestRuleFromBody(t *testing.T) {
 	testModule := `package a.b.c
 
-pi = 3.14159 { true }
+pi = 3.14159
 p[x] { x = 1 }
-greeting = "hello" { true }
-cores = [{0: 1}, {1: 2}] { true }
-wrapper = cores[0][1] { true }
-pi = [3, 1, 4, x, y, z] { true }
+greeting = "hello"
+cores = [{0: 1}, {1: 2}]
+wrapper = cores[0][1]
+pi = [3, 1, 4, x, y, z]
 foo["bar"] = "buz"
 foo["9"] = "10"
 foo.buz = "bar"
@@ -1860,10 +1860,35 @@ d1 := 1234
 		},
 	})
 
+	// Verify the rule and rule and rule head col/loc values
+	module, err := ParseModule("test.rego", testModule)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := range module.Rules {
+		col := module.Rules[i].Location.Col
+		if col != 1 {
+			t.Fatalf("expected rule %v column to be 1 but got %v", module.Rules[i].Head.Name, col)
+		}
+		row := module.Rules[i].Location.Row
+		if row != 3+i { // 'pi' rule stats on row 3
+			t.Fatalf("expected rule %v row to be %v but got %v", module.Rules[i].Head.Name, 3+i, row)
+		}
+		col = module.Rules[i].Head.Location.Col
+		if col != 1 {
+			t.Fatalf("expected rule head %v column to be 1 but got %v", module.Rules[i].Head.Name, col)
+		}
+		row = module.Rules[i].Head.Location.Row
+		if row != 3+i { // 'pi' rule stats on row 3
+			t.Fatalf("expected rule head %v row to be %v but got %v", module.Rules[i].Head.Name, 3+i, row)
+		}
+	}
+
 	mockModule := `package ex
 
-input = {"foo": 1} { true }
-data = {"bar": 2} { true }`
+input = {"foo": 1}
+data = {"bar": 2}`
 
 	assertParseModule(t, "rule name: input/data", mockModule, &Module{
 		Package: MustParsePackage(`package ex`),
@@ -2434,7 +2459,7 @@ func TestParserText(t *testing.T) {
 
 func TestRuleText(t *testing.T) {
 	input := ` package test
-	
+
 r[x] = y {
 	x = input.a
 	x = "foo"
@@ -2445,7 +2470,7 @@ r[x] = y {
 	x = input.c
 	x = "baz"
 }
-	
+
 r[x] = y {
 	x = input.d
 	x = "qux"

server/server_test.go
@@ -956,7 +956,7 @@ p = true { false }`
                   "location": {
                     "file": "testmod",
                     "row": 2,
-                    "col": 5
+                    "col": 1
                   }
                 }
               ]
@@ -3762,6 +3762,7 @@ func (f *fixture) reset() {
 }
 
 func executeRequests(t *testing.T, reqs []tr) {
+	t.Helper()
 	f := newFixture(t)
 	for i, req := range reqs {
 		if err := f.v1(req.method, req.path, req.body, req.code, req.resp); err != nil {
@@ -3772,6 +3773,7 @@ func executeRequests(t *testing.T, reqs []tr) {
 
 // Runs through an array of test cases against the v0 REST API tree
 func executeRequestsv0(t *testing.T, reqs []tr) {
+	t.Helper()
 	f := newFixture(t)
 	for i, req := range reqs {
 		if err := f.v0(req.method, req.path, req.body, req.code, req.resp); err != nil {